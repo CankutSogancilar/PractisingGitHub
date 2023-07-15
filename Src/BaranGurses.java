de838e16eedec8df2d6edeec287bdfe6ab685c00
Baran Gurses <barangurses39@hotmail.com>
2023-07-14 21:04:21 -0400

    hello

--- Src/BaranGurses.java
+++ Src/BaranGurses.java
@@ -3,5 +3,6 @@ package Src;
 public class BaranGurses {
     public static void main(String[] args) {
         System.out.println("cankut van damme");
+        System.out.println("Bruce Lee");
     }
 }
